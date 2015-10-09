fda5b184073e6b226c800c62dbca6e6af4e7964d
Jack Vaughn <jack.vaughn@icloud.com>
2015-10-09 14:26:21 -0400

    Initial Commit

---
+++ Pingit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pingit
+//
+//  Created by Vaughn, Jack on 10/9/15.
+//  Copyright © 2015 Rutherford County Schools. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pingit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pingit
+//
+//  Created by Vaughn, Jack on 10/9/15.
+//  Copyright © 2015 Rutherford County Schools. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
